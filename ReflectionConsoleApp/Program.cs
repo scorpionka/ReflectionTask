d68ad7a5baa908ae0a85f43c95eccd0ca39c87f0
Aliaksei_Kuzmitski <Aliaksei_Kuzmitski@EPBYMINW00F9>
2022-01-17 23:52:20 +0300

    Add project files.

---
+++ ReflectionConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
